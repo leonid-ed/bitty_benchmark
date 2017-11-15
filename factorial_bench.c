3fbf9e4466a0db835cc4fe0c23492bfe3a8a28a7
Leonid Edrenkin <leonid.ed@gmail.com>
2017-11-15 10:57:01 +0100

    factorial.c: Added comparison with numbers set in advance

--- factorial_bench.c
+++ factorial_bench.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
 #include "bitty_benchmark.h"
 
-static uint64_t factorial_table[] = {
+static const uint64_t _factorial_table[] = {
   1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800,
   479001600, 6227020800, 87178291200, 1307674368000, 20922789888000,
   355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000
 };
 
 #define ITERATIONS (1 << 24)
-#define NUMBERS (sizeof(factorial_table)/sizeof(uint64_t))
+#define NUMBERS (sizeof(_factorial_table)/sizeof(uint64_t))
 
 /* Factorial algorithm with recursion */
 static uint64_t
 factorial(int i)
 {
-  return factorial_table[i];
+  return _factorial_table[i];
 }
 
 static uint64_t
@@ -55,6 +56,7 @@ int main(int argc, char const *argv[])
   for (int i = 0; i < ITERATIONS; i++) {
     int t = i % NUMBERS;
     uint64_t f = factorialr(t);
+    assert(_factorial_table[t] == f);
 
     // STOP_BENCH(one);
     // fprintf(stderr, "%d: factorial(%d) = %ld\n", i, t, f);
@@ -79,7 +81,8 @@ int main(int argc, char const *argv[])
   RESTART_BENCH_WITH_DESC(one, "Factorial with memoization");
   for (int i = 0; i < ITERATIONS; i++) {
     int t = i % NUMBERS;
-    long f = factorial_memo(t);
+    uint64_t f = factorial_memo(t);
+    assert(_factorial_table[t] == f);
   }
   STOP_BENCH_AND_PRINT_RESULTS(one);
 
